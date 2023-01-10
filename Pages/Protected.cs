f10b7597f660858a837f79a62d4098781005e43c
gary.archer <gary.archer@WORKMAC.local>
2023-01-10 12:22:20 +0100

    Minor updates

--- Pages/Protected.cs
+++ Pages/Protected.cs
@@ -1,21 +1,49 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OidcClientDemoApplication;
 
 [Authorize]
 public class ProtectedModel : PageModel
 {
     public string Username { get; set; }
     public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+
+    private readonly TokenClient tokenClient;
+    
+    public ProtectedModel(TokenClient tokenClient)
+    {
+        this.tokenClient = tokenClient;
+    }
 
     public async Task OnGet()
     {
         ClaimsPrincipal user = this.User;
-        this.Username = user.FindFirstValue("sub");
+        var givenName = user.FindFirstValue("given_name");
+        var familyName = user.FindFirstValue("family_name");
+        this.Username = $"{givenName} {familyName}";
+        
+        this.AccessToken = await this.tokenClient.GetAccessToken(this.HttpContext);
+        this.RefreshToken = await this.tokenClient.GetRefreshToken(this.HttpContext);
+    }
 
-        // Get the access token required for upstream requests
-        this.AccessToken = await this.HttpContext.GetTokenAsync("access_token");
+    public async Task<IActionResult> OnPostRefreshToken()
+    {
+        await this.tokenClient.RefreshAccessToken(this.HttpContext);
+        this.AccessToken = await this.tokenClient.GetAccessToken(this.HttpContext);
+        this.RefreshToken = await this.tokenClient.GetRefreshToken(this.HttpContext);
+        return Page();
+    }
+
+    public async Task OnPostLogout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,9 @@
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-//using System.Net;
-//using System.Net.Http;
-//using System.Threading.Tasks;
+// using System.Net;
+// using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,6 +11,8 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 namespace OidcClientDemoApplication
 {
@@ -37,11 +39,9 @@ namespace OidcClientDemoApplication
             });
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // Prevent WS-Federation claim names being added to tokens
+            // Prevent WS-Federation claim names being written to tokens
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             
             services.AddAuthentication(options => {
@@ -51,55 +51,65 @@ namespace OidcClientDemoApplication
             })
             .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options => {
                 
-                // This is the strongest setting in production and also supports HTTP on developer workstations
+                // Use the strongest setting in production, which also enables HTTP on developer workstations
                 options.Cookie.SameSite = SameSiteMode.Strict;
             })
             .AddOpenIdConnect(options => {
 
-                // Use the same settings here
+                // Use the same settings for temporary cookies
                 options.NonceCookie.SameSite = SameSiteMode.Strict;
                 options.CorrelationCookie.SameSite = SameSiteMode.Strict;
-                
                 options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.Authority=Configuration.GetValue<string>("OpenIdConnect:Issuer");
-                options.ClientId=Configuration.GetValue<string>("OpenIdConnect:ClientId");
-                options.ClientSecret=Configuration.GetValue<string>("OpenIdConnect:ClientSecret");
-                options.ResponseType = "code";
-                options.SaveTokens = true;
                 
-                // Add scope to the request if configured; "openid profile" is default
+                // Set the main OpenID Connect settings
+                options.Authority = Configuration.GetValue<string>("OpenIdConnect:Issuer");
+                options.ClientId = Configuration.GetValue<string>("OpenIdConnect:ClientId");
+                options.ClientSecret = Configuration.GetValue<string>("OpenIdConnect:ClientSecret");
+                options.ResponseType = OpenIdConnectResponseType.Code;
+                options.ResponseMode = OpenIdConnectResponseMode.Query;
                 string scopeString = Configuration.GetValue<string>("OpenIDConnect:Scope");
-                if (!String.IsNullOrEmpty(scopeString)) {
-                    
-                    options.Scope.Clear();
-                    scopeString.Split(" ", StringSplitOptions.TrimEntries).ToList().ForEach(scope => {
-                        options.Scope.Add(scope);
-                    });
-                }
+                options.Scope.Clear();
+                scopeString.Split(" ", StringSplitOptions.TrimEntries).ToList().ForEach(scope => {
+                    options.Scope.Add(scope);
+                });
 
-                // Initial configuration is HTTP
+                // If required, override the issuer and audience used to validate ID tokens
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = options.Authority,
+                    ValidAudience = options.ClientId
+                };
+
+                // This example gets user information for display from the user info endpoint
+                options.GetClaimsFromUserInfoEndpoint = true;
+
+                // Handle the post logout redirect URI
+                options.Events.OnRedirectToIdentityProviderForSignOut = (context) =>
+                {
+                    context.ProtocolMessage.PostLogoutRedirectUri = Configuration.GetValue<string>("OpenIdConnect:PostLogoutRedirectUri");
+                    return Task.CompletedTask;
+                };
+                
+                // Save tokens issued to encrypted cookies
+                options.SaveTokens = true;
+
+                // Set this in developer setups if the OpenID Provider uses plain HTTP
                 options.RequireHttpsMetadata = false;
 
-                // For testing, enforce login even if user has an alive session with the Curity Identity Server
-                options.Prompt = "login"; 
-
+                /* Uncomment to debug HTTP requests from the web backend to the Identity Server
+                   Run a tool such as MITM proxy to view the request and response messages
                 /*options.BackchannelHttpHandler = new HttpClientHandler()
                 {
                     Proxy = new WebProxy("http://127.0.0.1:8888"),
                     UseProxy = true,
-                };
-
-                options.Events.OnMessageReceived = ctx => {
-
-                    System.Console.WriteLine("*** Message received");
-                    System.Console.WriteLine(ctx.Response);
-                    return Task.CompletedTask;
                 };*/
-
             });
 
             services.AddAuthorization();
             services.AddRazorPages();
+
+            // Add this app's types to dependency injection
+            services.AddSingleton<TokenClient>();
         }
     }
 }
